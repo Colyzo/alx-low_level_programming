5977cf6e10491a7cc4f3e43dc9418af2d0acf502
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-06-30 15:04:16 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,14 +4,14 @@
 *C Programming by O.I Collins
 * Description: main - Prints out a string to stdout.
 * Return: 0 if success.
-**/
+*/
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }

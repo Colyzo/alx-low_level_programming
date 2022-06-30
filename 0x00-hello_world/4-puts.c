9b6c1427a606510b48f54f9a6f6d2297be70fd20
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-06-30 23:10:29 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
-* C Programming by O.I Collins
 * Description: main - Prints out a string to stdout.
 * Return: 0 if success.
 */
 
-int main (void)
+int main(void)
 
 {
 

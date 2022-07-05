069072286b173eade125b16ba54e145b21e3f319
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-07-05 13:18:26 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: print _putchar followed on a new line
+ * Return: always 0
+ */
+int main(void)
+{
+README.md _putchar(_);
+README.md _putchar(p);
+README.md _putchar(u);
+README.md _putchar(t);
+README.md _putchar(c);
+README.md _putchar(h);
+README.md _putchar(a);
+README.md _putchar(r);
+README.md _putchar(n);
+
+README.md return (0);
+}

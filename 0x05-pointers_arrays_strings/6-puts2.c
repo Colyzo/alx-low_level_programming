459789dd43aa07eca56e4fa56290082ad59cd52c
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-07-12 20:01:35 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,16 @@
  */
 void puts2(char *str)
 {
-int c;
-char l;
+	int c;
+	char l;
 
-for (c = 0; str[c] != 0; c++)
-{
-if (c % 2 == 0)
-{
-l = str[c];
-_putchar(l);
-}
-}
-_putchar(n);
+	for (c = 0; str[c] != 0; c++)
+	{
+		if (c % 2 == 0)
+		{
+			l = str[c];
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
 }

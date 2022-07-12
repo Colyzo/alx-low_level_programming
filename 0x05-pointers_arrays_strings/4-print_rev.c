8f94715f76f3f2e6e20bab680cc21f6efcf9e698
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-07-12 19:53:18 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,16 @@
  */
 void print_rev(char *s)
 {
-int i = 0;
+	int i = 0;
 
-while (*(s + i))
-i++;
-i = i - 1;
-while (i >= 0)
-{
-_putchar(*(s + i));
-i--;
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	
 }
-_putchar(n);
+	_putchar('\n');
 }

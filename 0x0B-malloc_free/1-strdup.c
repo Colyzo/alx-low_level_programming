18dd367b08b644d9debf16c66aab4d2a0de25318
Ofeimu Imo-obome Collins <ofeimucollins@gmail.com>
2022-07-25 16:00:21 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
@@ -9,22 +9,21 @@
  */
 char *_strdup(char *str)
 {
-unsigned int i, j;
-char *s;
+	    unsigned int i, j;
+	    char *s;
 
-if (str == NULL)
-return (NULL);
-for (i = 0; str[i]; i++)
-;
-i++;
-s = malloc(i * sizeof(char));
-if (s == NULL)
-{
-return (NULL);
-}
-for (j = 0; j < i; j++)
-{
-s[j] = str[j];
-}
-return (s);
+	    if (str == NULL)
+		  return (NULL);
+	    for (i = 0; str[i]; i++);
+    	i++;
+    	s = malloc(i * sizeof(char));
+	    if (s == NULL)
+	      {
+	        	return (NULL);
+	      }
+	    for (j = 0; j < i; j++)
+	      {
+		        s[j] = str[j];
+	      }
+	return (s);
 }
